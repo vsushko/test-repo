9972dd9078b3018d7a7dea0bd27fb23270adab6b
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2024-02-06 15:10:21 +0400

    aded test repo

---
+++ src/test/java/ae/rakbank/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ae.rakbank.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
