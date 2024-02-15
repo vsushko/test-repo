c5c9f1ef784398c817e4b9433da4462dd3e87956
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2024-02-15 15:58:30 +0400

    added gradle example

--- README.md
+++
@@ -1,1 +0,0 @@
-https://rakartifactory.jfrog.io/ui/repos/tree/General/backend-local/gradle-8.6-all.zip
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package ae.rakbank.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
